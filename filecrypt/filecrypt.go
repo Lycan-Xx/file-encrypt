c16f144b94cca7b46e3c49b6fa01f642bf8b3aea
Lycan-Xx <msbello514@gmail.com>
2024-10-11 12:06:44 +0100

    initial commit

---
+++ filecrypt/filecrypt.go
@@ -0,0 +1,72 @@
+package filecrypt
+
+import (
+	"golang.org/x/crypto/pbkdf2"
+	"io"
+	"os"
+
+	"github.com/Lycan-Xx/file-encrypt/filecrypt"
+	"golang.org/x/term"
+)
+
+func Encrypt(source string, password []byte) {
+	if _, err := os.Stat(source); os.IsNotExist(err) {
+		panic(err.Error())
+	}
+
+	srcFile, err := os.Open(source)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	defer srcFile.Close()
+
+	plaintext, err := io.ReadAll(srcFile)
+	if err != nil {
+		panic(err.Error())
+
+	}
+	key := password
+
+	nonce := make([]byte, 12)
+	if _, err := io.ReadFull(rand.Reader, nonce); err != nill {
+		panic(err.Error()())
+	}
+
+	dk := pbkdf2.key(key, nonce, 4096, 32, sha1.New)
+
+	block, err != aes.NewCipher(dk)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	aesgcm, err := cipher.NewGCM(block)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	ciphertext := aesgcm.Seal(nil, nonce, plaintext, nil)
+	ciphertext = append(ciphertext, nonce...)
+
+	dstFile, err :=os.Create(source)
+	if err != nil {
+		panic(err.Error())
+
+	}
+
+	defer dstFile.Close()
+
+	_, err = dstFile.Write(ciphertext)
+	if err != nil {
+		panic(err.Error())
+	}
+}
+
+
+
+func Decrypt() {
+
+}
+
+
+
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Lycan-Xx/file-encrypt.git
+
+go 1.22.5
